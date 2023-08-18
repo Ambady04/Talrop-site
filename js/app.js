5b6d1edf48e1d29b42cedde88d60d316dd16ec0a
Ambady <ambady.talrop@gmail.com>
2023-08-18 09:36:13 +0530

    Title: initial commit

--- js/app.js
+++ js/app.js
@@ -1,27 +1,17 @@
-// window.addEventListener("wheel", event => {
-//     const delta = Math.sign(event.deltaY);
-//     console.info(typeof(delta));
+window.addEventListener("wheel", event => {
+    const delta = Math.sign(event.deltaY);
+    console.log(event.deltaY);
 
 
-//     if (delta === 1) {
-//         document.querySelector("#ecosystem > div > div.left").classList.add("slide-right");
-//         console.log("shameems")
-//     } else {
-//         document.querySelector("#ecosystem > div > div.left").classList.remove("slide-right");
-//     }
-// });
-
-$(window).scroll(function(){
-
-    var scroll = $(window).scrollTop();
-    console.log(scroll)
-
-    if (scroll >= 1100) {
-        let element = $("#ecosystem > div > div.left");
-        element.addClass("slide-right")
-    } else if (scroll < 1100) {
-        let element = $("#ecosystem > div > div.left");
-        element.removeClass("slide-right")
-
+    if (event.deltaY >= 1.25 ) {
+        document.querySelector("#home > header").style.position = "static";
+        document.querySelector("#spotlight > div.wrapper").style.marginTop = "initial"
+        
+        
+    } else if (event.deltaY <= -1.25 ) {
+        document.querySelector("#home > header").style.position = "fixed";
+        document.querySelector("#spotlight > div.wrapper").style.marginTop = "100px"
+        document.querySelector("#home > header").classList.add("down aos-init")
+        
     }
-})
+});
